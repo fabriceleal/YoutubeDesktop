22c61cff968cae7e51fff696b4067191243da43d
Fabrice Ferreira Leal <fabrice.leal.ch@gmail.com>
2011-12-13 20:47:08 +0000

    Author structure

--- YoutubeDesktop/Youtube/UriYoutube/UriYoutubeAuthor.cs
+++ YoutubeDesktop/Youtube/UriYoutube/UriYoutubeAuthor.cs
@@ -5,8 +5,28 @@ using System.Text;
 
 namespace YoutubeDesktop.Youtube.UriYoutube
 {
-    public class UriYoutubeAuthor
+    public class UriYoutubeAuthor : UriYoutubeBase
     {
-        // TODO: ...
+        public UriYoutubeAuthor(String address) : base(address, ParamYoutube.Empty) { }
+        public UriYoutubeAuthor(String address, ParamYoutube pars) : base(address, pars) { }
+
+        public override RawYoutubeType DoQuery()
+        {
+            // TODO: Implement
+            return null;
+        }
+
+        public override Factory.IFactory GetFactory()
+        {
+            // TODO: Implement
+            return null;
+        }
+
+        public override IQueryUIObject GetUINavigatorObject()
+        {
+            // TODO: Implement
+            return null;
+        }
+
     }
 }
